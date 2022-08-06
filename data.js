9dbbb6151bfd0c99333b844f3c3969cb368665d2
Robson Narcizo <rob.narcizo.27@gmail.com>
2022-08-06 07:04:02 -0300

    Treinameto Fundamentos

---
+++ data.js
@@ -0,0 +1,27 @@
+module.exports = {
+    funcionarios: [
+        {
+            id: 1,
+            nome: 'Robson'
+        },
+        {
+            id: 2,
+            nome: 'Pedro'
+        },
+        {
+            id: 3,
+            nome: 'Jean'
+        },
+        {
+            id: 4,
+            nome: 'Rodrigo'
+        },
+        {
+            id: 5,
+            nome: 'Cludio'
+        }
+    ],
+    name:'Robson'
+};
+
+
---
+++ teste.js
@@ -0,0 +1,8 @@
+const data = require('./data');
+
+
+
+
+
+
+
